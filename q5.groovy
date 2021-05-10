868b436964fb6c6d44d69d406f98c4b5953a34bc
paz <pazelbert@gmail.com>
2021-05-10 14:47:33 +0300

    added groovy script for q5

---
+++ q5.groovy
@@ -0,0 +1,10 @@
+ def get = new URL("localhost:5000").openConnection();
+ def getRC = get.getResponseCode();
+ println(getRC);
+ if(getRC.equals(200)) {
+    println(get.getInputStream().getText());
+    println('OK')
+ else{
+ 	println('NOT OK')
+ 	} 
+ }
